184d8b1666b0b627d2e24a2d21b8bd07e79f5fa2
Bogus <bogus@url.com>
2023-01-29 21:13:15 +0200

    Create new app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib nime
+//Rakendus tervitab nimepidi
+
+Console.WriteLine("Enter your name:");
+//string-sõne
+string? userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+
+Console.WriteLine($"Hello, + {userName}!");
